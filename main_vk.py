00902dbf4cad789f69e77e61d3d7bd272eb7f269
Захаров Павел <pzakharov83@gmail.com>
2022-01-31 17:54:11 +0300

    skip response in VK bot if no response

--- main_vk.py
+++ main_vk.py
@@ -24,11 +24,12 @@ def echo(event, vk_api):
         session_id=event.user_id,
         text=event.text
     )
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=response,
-        random_id=random.randint(1, 1000)
-    )
+    if response:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response,
+            random_id=random.randint(1, 1000)
+        )
 
 
 def detect_intent_texts(project_id, session_id, text, language_code='ru'):
@@ -53,6 +54,8 @@ def detect_intent_texts(project_id, session_id, text, language_code='ru'):
         f'(confidence {response.query_result.intent_detection_confidence})'
     )
     print(f'Fullfillment text: {response.query_result.fulfillment_text}')
+    if response.query_result.intent.is_fallback:
+        return None
     return response.query_result.fulfillment_text
 
 
